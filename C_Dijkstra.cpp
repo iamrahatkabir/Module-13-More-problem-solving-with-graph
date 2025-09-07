0b8b2c548b29aabc3a88b13fbf44b9c14eea8b91
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-09-08 02:29:50 +0600

    Dijkstra Path Printing

---
+++ C_Dijkstra.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int //Make long long int short using #define 
+
+// https://codeforces.com/problemset/problem/20/C
+
+vector<pair<ll,ll>> adj_list [100005];
+ll dis[100005];
+ll parent[100005];
+
+void dijkstra(ll src)
+{
+    priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
+    pq.push({0, src});
+    dis[src] = 0;
+
+    while(!pq.empty())
+    {
+        pair<ll,ll> par = pq.top();
+        pq.pop();
+
+        ll par_node = par.second;
+        ll par_dis = par.first;
+
+        for(auto child : adj_list[par_node])
+        {
+            ll child_node = child.first;
+            ll child_dis = child.second;
+
+            if(par_dis + child_dis < dis[child_node])
+            {
+                dis[child_node] = par_dis + child_dis;
+                pq.push({dis[child_node], child_node});
+                parent[child_node] = par_node;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    while(e--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        adj_list[a].push_back({b, c});
+        adj_list[b].push_back({a, c});
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        dis[i] = LLONG_MAX;
+        parent[i] = -1;
+    }
+
+    dijkstra(1);
+
+    if(dis[n] == LLONG_MAX)
+        cout << -1 << endl;
+    else
+    {
+        ll node = n;
+        vector<ll> path;
+
+        while(node != -1)
+        {
+            path.push_back(node);
+            node = parent[node];
+        }
+
+        reverse(path.begin(), path.end());
+        for(auto x : path)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+
+
+    }
+
+    
+    
+    return 0;
+}
